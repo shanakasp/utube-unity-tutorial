f59c0b223e618d9aeabf247e4dbc03ac56456d0f
Thisara Shanaka Prince <74164029+shanakasp@users.noreply.github.com>
2024-08-04 10:50:00 +0530

    Update 2ndgamePlayerControl.cs

--- 2ndgamePlayerControl.cs
+++ 2ndgamePlayerControl.cs
@@ -1,10 +1,8 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     GameManger _gameManager;
     Rigidbody2D _rb;
     Camera _mainCamera;
@@ -16,12 +14,14 @@ public class PlayerController : MonoBehaviour
     Vector2 _offSet;
 
     [SerializeField] GameObject _bullet;
-    [SerializeField] GameObject _bulletSpawn;
+    [SerializeField] Transform _bulletSpawn;
 
     bool _isShooting = false;
     float _ballSpeed = 15f;
 
-    // Start is called before the first frame update
+    float _moveHorizontal;
+    float _moveVertical;
+
     void Start()
     {
         _rb = gameObject.GetComponent<Rigidbody2D>();
@@ -31,6 +31,59 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _moveHorizontal = Input.GetAxisRaw("Horizontal");
+        _moveVertical = Input.GetAxisRaw("Vertical");
+
+        _moveVelocity = new Vector2(_moveHorizontal, _moveVertical) * _moveSpeed;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _isShooting = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+        RotatePlayer();
+
+        if (_isShooting)
+        {
+            StartCoroutine(Fire());
+            _isShooting = false;
+        }
+    }
+
+    void MovePlayer()
+    {
+        if (_moveHorizontal != 0 || _moveVertical != 0)
+        {
+            if (_moveHorizontal != 0 && _moveVertical != 0)
+            {
+                _moveVelocity *= _speedLimiter;
+            }
+
+            _rb.velocity = _moveVelocity;
+        }
+        else
+        {
+            _rb.velocity = Vector2.zero;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        _mousePos = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDir = _mousePos - _rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        _rb.rotation = angle;
+    }
+
+    IEnumerator Fire()
+    {
+        GameObject bullet = Instantiate(_bullet, _bulletSpawn.position, _bulletSpawn.rotation);
+        Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+        bulletRb.AddForce(_bulletSpawn.up * _ballSpeed, ForceMode2D.Impulse);
+        yield return null;
     }
 }
